9e2ebef5bc4b64a06ba188aee65ef951b46af485
mohamedamr99t <mohamedamr99t@gmail.com>
2023-11-04 20:38:36 +0200

    Final Project Initialzation

---
+++ terraform/startup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo apt-get install -y
+sudo apt install docker.io 
+sudo groupadd docker
+sudo usermod -aG docker $USER
+sudo apt-get install -y kubectl
