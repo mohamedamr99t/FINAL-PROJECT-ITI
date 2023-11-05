587a5cab0aa8338e214b14dd0fb9303266618b59
mohamedamr99t <mohamedamr99t@gmail.com>
2023-11-05 13:52:28 +0200

    index.html creation

--- application/helloworld.py
+++ application/helloworld.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, send_file
 
-app = Flask(__name__)
+app = Flask(__name)
 
 @app.route('/')
 def hello():
-    return "Hello, World! , From the final project "
+    return send_file('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
