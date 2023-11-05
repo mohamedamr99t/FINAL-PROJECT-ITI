9122b5b730a7a565b6c462578cb6ce2f4f8d3f4f
mohamedamr99t <mohamedamr99t@gmail.com>
2023-11-05 13:11:24 +0200

    -

--- terraform/iam/main.tf
+++ terraform/iam/main.tf
@@ -10,13 +10,14 @@ resource "google_service_account" "gke_sa" {
 }
 
 resource "google_project_iam_binding" "service-accounts-binding-management" {
+  count   = length(var.vm_roles)
   project = var.project_id
   role    = var.vm_roles[count.index]
-
   members = [google_service_account.management_sa.member]
 }
 
 resource "google_project_iam_binding" "service-accounts-binding-gke" {
+  count   = length(var.gke_roles)
   project = var.project_id
   role    = var.gke_roles[count.index]
   members = [google_service_account.gke_sa.member]
