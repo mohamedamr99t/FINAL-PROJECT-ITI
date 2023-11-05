3649a8913e0d7d3d092504236c56a67c63a59c81
Mohamed Amr <139065575+mohamedamr99t@users.noreply.github.com>
2023-11-05 04:43:18 +0200

    Update main.tf

--- terraform/gke/main.tf
+++ terraform/gke/main.tf
@@ -31,7 +31,6 @@ resource "google_container_cluster" "gke_cluster" {
   private_cluster_config {
     enable_private_endpoint = false
     enable_private_nodes    = false
-    master_ipv4_cidr_block  = "172.16.0.0/28"
 
     master_global_access_config {
       enabled = true
