8c4eeb347ad3a19ea15a6deb99c027a13f22417f
mohamedamr99t <mohamedamr99t@gmail.com>
2023-11-05 05:20:54 +0200

    rename custom roles names

--- terraform/iam/main.tf
+++ terraform/iam/main.tf
@@ -9,27 +9,27 @@ resource "google_service_account" "gke_sa" {
   display_name = "GKE Service Account"
 }
 
-resource "google_project_iam_custom_role" "management_role-x" {
-  role_id     = "management_role_demo-x"
+resource "google_project_iam_custom_role" "management_role_x" {
+  role_id     = "management_role_demo_x"
   title       = "Management Service Account Role"
   permissions = var.permissions-roles-management
 }
 
-resource "google_project_iam_custom_role" "gke_role-x" {
-  role_id     = "gke_role_demo-x"
+resource "google_project_iam_custom_role" "gke_role_x" {
+  role_id     = "gke_role_demo_xx"
   title       = "GKE Service Account Role"
   permissions = var.permissions-roles-gke
 }
 
 resource "google_project_iam_binding" "service-accounts-binding-management" {
   project = var.project_id
-  role    = google_project_iam_custom_role.management_role.id
+  role    = google_project_iam_custom_role.management_role_x.id
 
   members = [google_service_account.management_sa.member]
 }
 
 resource "google_project_iam_binding" "service-accounts-binding-gke" {
   project = var.project_id
-  role    = google_project_iam_custom_role.gke_role.id
+  role    = google_project_iam_custom_role.gke_role_x.id
   members = [google_service_account.gke_sa.member]
 }
